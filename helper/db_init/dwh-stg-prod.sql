589dbb91ac1016a3bda3a1a7389482519f75d7f8
rahilaode <laoderahilatansa@gmail.com>
2024-03-13 13:51:09 +0700

    update schema for staging and prod

--- helper/db_init/dwh-stg-prod.sql
+++ helper/db_init/dwh-stg-prod.sql
@@ -6,72 +6,66 @@ CREATE SCHEMA IF NOT EXISTS prod AUTHORIZATION postgres;
 
 -- Staging
 CREATE TABLE stg.category (
-    uuid uuid default uuid_generate_v4(),
-    category_id serial4 NOT NULL,
+    id uuid default uuid_generate_v4(),
+    category_id serial primary key NOT NULL,
     "name" varchar(255) NOT NULL,
     description text NULL,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-    CONSTRAINT category_pkey PRIMARY KEY (category_id)
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL
 );
 
 CREATE TABLE stg.customer (
-    uuid uuid default uuid_generate_v4(),
-    customer_id serial4 NOT NULL,
+    id uuid default uuid_generate_v4(),
+    customer_id serial primary key NOT NULL,
     first_name varchar(255) NOT NULL,
     last_name varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
     phone varchar(100) NULL,
     address text NULL,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-    CONSTRAINT customer_pkey PRIMARY KEY (customer_id)
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL
 );
 
 CREATE TABLE stg.orders (
-    uuid uuid default uuid_generate_v4(),
-    order_id varchar(50) NOT NULL,
+    id uuid default uuid_generate_v4(),
+    order_id varchar(50) primary key NOT NULL,
     customer_id int4 NULL,
     order_date timestamp NOT NULL,
     status varchar(50) NOT NULL,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-    CONSTRAINT orders_pkey PRIMARY KEY (order_id)
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL
 );
 
 CREATE TABLE stg.order_detail (
-    uuid uuid default uuid_generate_v4(),
-    order_detail_id serial4 NOT NULL,
+    id uuid default uuid_generate_v4(),
+    order_detail_id serial primary key NOT NULL,
     order_id varchar(50) NULL,
     product_id varchar(100) NULL,
     quantity int4 NOT NULL,
     price numeric(10, 2) NOT NULL,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-    CONSTRAINT order_detail_pkey PRIMARY KEY (order_detail_id)
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL
 );
 
 CREATE TABLE stg.subcategory (
-    uuid uuid default uuid_generate_v4(),
-	subcategory_id serial4 NOT NULL,
+    id uuid default uuid_generate_v4(),
+	subcategory_id serial primary key NOT NULL,
 	"name" varchar(255) NOT NULL,
 	category_id int4 NULL,
 	description text NULL,
 	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-	CONSTRAINT subcategory_pkey PRIMARY KEY (subcategory_id)
+	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL
 );
 
 CREATE TABLE stg.product (
-    uuid uuid default uuid_generate_v4(),
-    product_id varchar(100) NOT NULL,
+    id uuid default uuid_generate_v4(),
+    product_id varchar(100) primary key NOT NULL,
     "name" text NOT NULL,
     subcategory_id int4 NULL,
     price numeric(10, 2) NOT NULL,
     stock int4 NOT NULL,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
-    CONSTRAINT product_pkey PRIMARY KEY (product_id)
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL
 );
 
 ALTER TABLE stg.orders
@@ -144,11 +138,11 @@ CREATE TABLE prod.dim_date(
     weekend_indr             VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE prod.fact_order(
-	order_id uuid primary key default uuid_generate_v4() ,
+CREATE TABLE prod.fct_order(
+	order_id uuid default uuid_generate_v4() ,
 	product_id uuid references prod.dim_product(product_id),
 	customer_id uuid references prod.dim_customer(customer_id),
-	order_date int references prod.dim_date(date_id),
+	date_id int references prod.dim_date(date_id),
 	quantity int,
 	status varchar(50),
 	created_at timestamp,
